3f49b0aa731430d784cc6a847f0873f4ec7904ba
Xristos9 <75035610+Xristos9@users.noreply.github.com>
2021-01-17 19:29:15 +0200

    Update user_set.php

--- user_set.php
+++ user_set.php
@@ -1,66 +1,42 @@
 <?php
-session_start();
+	include "db_conn.php";
 
-if(isset($_SESSION['userID']) && isset($_SESSION['userName'])){
-?>
+	session_start();
+	$userID=$_SESSION["userID"];
 
+    $result = mysqli_query($conn,"SELECT serverIPAddress FROM hardata INNER JOIN ips ON hardata.entryID = ips.entryID WHERE `ips`.`userID`=$userID AND (RsContentType like '%html%' OR RsContentType like '%javascript%' OR RsContentType like '%php%' OR RqContentType like '%html%' OR RqContentType like '%javascript%' OR RqContentType like '%php%') ORDER BY `hardata`.`serverIPAddress` ASC");
+	$servers= array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			array_push($servers,$row['serverIPAddress']);
+		}
+	}
+	// print_r($userID);
+	
+	$result2 = mysqli_query($conn,"SELECT Date FROM ips WHERE `userID`=$userID ORDER BY `ips`.`Date` ASC");
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- Sympols -->
-
-        <title>Home </title>
-    
-        <link rel="stylesheet" type="text/css" href="style_user.css"> 
-    </head>
-    
-    <body>
-    
-
-    <?php
-        include "header.php";
-    ?>
-
-
-
-         <h1>Hello, <?php echo $_SESSION['userName']; ?></h1>
-
-        <nav class="user-nav">
-            <a href="change-password.php">Change Password</a>
-            <a href="change-username.php">Change Username</a> 
-            <a href="logout.php">Logout</a>
-        </nav>
-
-        <br>
-        <br>
-        <br>
-        
-        <!-- page wrapper -->
-        <div class="page-wrapper">
-            
-        </div>
-        <!-- /page wrapper -->
-
-        <!-- page footer -->
-     <?php
-        include "footer.php";
-    ?>
-    <!-- /page footer -->
-   
-    </body>
+	$dates= array();
+	if ($result2->num_rows > 0) {
+		// output data of each row
+		while($row = $result2->fetch_assoc()) {
+			array_push($dates,$row['Date']);
+		}
+	}
+	$_SESSION["uDate"]=end($dates);
+	// print_r($_SESSION["uDate"]);
 
 
+	
+	$result3 = mysqli_query($conn,"SELECT entryID FROM `ips` WHERE `userID`=$userID");
+	$entries= array();
+	if ($result3->num_rows > 0) {
+		// output data of each row
+		while($row = $result3->fetch_assoc()) {
+			array_push($entries,$row['entryID']);
+		}
+	}
+	$_SESSION["nOfEntries"]=(count($entries));
 
-</html>
-
-<?php
-} else{
-    header("Location: index.php");
-    exit();
-}
+	
 ?>
