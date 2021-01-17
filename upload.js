2c1719189f2096a436398bae4b95e7868763d22f
Xristos9 <75035610+Xristos9@users.noreply.github.com>
2021-01-17 19:30:48 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,42 +1,137 @@
-<?php
-	include "db_conn.php";
+var E= []
 
-	session_start();
-	$userID=$_SESSION["userID"];
-
-    $result = mysqli_query($conn,"SELECT serverIPAddress FROM hardata INNER JOIN ips ON hardata.entryID = ips.entryID WHERE `ips`.`userID`=$userID AND (RsContentType like '%html%' OR RsContentType like '%javascript%' OR RsContentType like '%php%' OR RqContentType like '%html%' OR RqContentType like '%javascript%' OR RqContentType like '%php%') ORDER BY `hardata`.`serverIPAddress` ASC");
-	$servers= array();
-	if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-			array_push($servers,$row['serverIPAddress']);
-		}
-	}
-	// print_r($userID);
+$.getJSON("http://ip-api.com/json/?fields=61439", 
+		function(data) { 
+			info={}                
+					
+			info.ip = data.query
+			info.lat = data.lat
+			info.lon = data.lon
+			info.par = data.isp
+			E.push(info)
+		})
+		
+function readFile(input) {
+	var file = new FileReader()
+	const read=file.readAsText(input.files[0])
 	
-	$result2 = mysqli_query($conn,"SELECT Date FROM ips WHERE `userID`=$userID ORDER BY `ips`.`Date` ASC");
-
-	$dates= array();
-	if ($result2->num_rows > 0) {
-		// output data of each row
-		while($row = $result2->fetch_assoc()) {
-			array_push($dates,$row['Date']);
+	file.onload = function(e) {
+		const data = JSON.parse(e.currentTarget.result)
+		//console.log(data)
+		
+		var url=0
+		const getDomain = (url) => {
+			url = new URL(url).hostname
+			return url;
 		}
-	}
-	$_SESSION["uDate"]=end($dates);
-	// print_r($_SESSION["uDate"]);
+
+		for (var i in data.log.entries) {
+			var entries = {}
+			url = data.log.entries[i].request.url
+			
+			entries.startedDateTime = data.log.entries[i].startedDateTime
+			entries.serverIPAddress = data.log.entries[i].serverIPAddress
+			entries.timingsWait= data.log.entries[i].timings.wait
+			entries.method = data.log.entries[i].request.method
+			entries.url = getDomain(url)
+			entries.status = data.log.entries[i].response.status
+			entries.statusText = data.log.entries[i].response.statusText
+			
+
+			for(var z in data.log.entries[i].request.headers){
+				
+				if(data.log.entries[i].request.headers[z].name === "host" || data.log.entries[i].request.headers[z].name === "Host"){
+					entries.host=(data.log.entries[i].request.headers[z].value)
+					
+				} 
+				
+				if(data.log.entries[i].request.headers[z].name === "content-type" || data.log.entries[i].request.headers[z].name === "Content-Type"){
+					entries.RqContentType=(data.log.entries[i].request.headers[z].value)
+				} 
+				if(data.log.entries[i].request.headers[z].name === "cache-control" || data.log.entries[i].request.headers[z].name === "Cache-Control"){
+				   entries.RqCacheControl=(data.log.entries[i].request.headers[z].value)
+				} 
+				if(data.log.entries[i].request.headers[z].name === "Expires" || data.log.entries[i].request.headers[z].name === "expires"){
+					entries.RqExpires=(data.log.entries[i].request.headers[z].value)
+				}
+				
+				
+			}
+			 
+			
+			for(var y in data.log.entries[i].response.headers){
+
+				if(data.log.entries[i].response.headers[y].name === "content-type" || data.log.entries[i].response.headers[y].name === "Content-Type"){
+					entries.RsContentType=(data.log.entries[i].response.headers[y].value)
+				}
+
+				if(data.log.entries[i].response.headers[y].name === "cache-control" || data.log.entries[i].response.headers[y].name === "Cache-Control"){
+					entries.RsCacheControl=(data.log.entries[i].response.headers[y].value)
+				}
+
+				if(data.log.entries[i].response.headers[y].name === "pragma" || data.log.entries[i].response.headers[y].name === "Pragma"){
+					entries.pragma=(data.log.entries[i].response.headers[y].value)
+				}
+
+				if(data.log.entries[i].response.headers[y].name === "expires" || data.log.entries[i].response.headers[y].name === "Expires"){
+					entries.RsExpires=(data.log.entries[i].response.headers[y].value)
+				}
+
+				if(data.log.entries[i].response.headers[y].name === "last-modified"  || data.log.entries[i].response.headers[y].name === "Last-Modified"){
+					entries.lastModified=(data.log.entries[i].response.headers[y].value)
+				}
+				if(data.log.entries[i].response.headers[y].name === "Age" || data.log.entries[i].response.headers[y].name === "age"){
+					entries.age=(data.log.entries[i].response.headers[y].value)
+				}
+			}
+			E.push(entries)
 
 
-	
-	$result3 = mysqli_query($conn,"SELECT entryID FROM `ips` WHERE `userID`=$userID");
-	$entries= array();
-	if ($result3->num_rows > 0) {
-		// output data of each row
-		while($row = $result3->fetch_assoc()) {
-			array_push($entries,$row['entryID']);
+			
 		}
-	}
-	$_SESSION["nOfEntries"]=(count($entries));
 
-	
-?>
+	   
+		console.log(E)
+	   
+
+		
+			
+
+		var myJSONText = JSON.stringify(E,null,2)
+		//console.log(myJSONText)
+		document.getElementById("up").addEventListener("click", function() {
+			$.ajax({ 
+				url: "upload.php",
+				type: "POST",  
+				data: {kati:myJSONText}, 
+				success: function(res) { 
+						console.log(res)
+						
+					} 
+			}); 
+		});
+	};
+
+
+	file.onerror = function() {
+		console.log(reader.error);
+	};
+
+}
+
+
+function onDownload(){
+	download(JSON.stringify(E,null,2), "json-file-name.json", "application/json");
+
+}
+   
+function download(content, fileName, contentType) {
+	const a = document.createElement("a");
+	const file = new Blob([content], { type: contentType });
+	a.href = URL.createObjectURL(file);
+	a.download = fileName;
+	a.click();
+}
+		   
+
+
